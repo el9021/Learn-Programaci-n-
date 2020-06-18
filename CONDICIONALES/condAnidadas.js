8d27c7e93f513c2e704344fba481910ee45dae32
Elvis Cornejo Ramos <elviscornejo1@gmail.com>
2020-06-18 16:13:30 +0000

    Initial commit

---
+++ CONDICIONALES/condAnidadas.js
@@ -0,0 +1,14 @@
+//CONDICIONALES MULTIPLES
+// Primero declaramos una variable
+let edad = 29
+let mayorDeEdad = edad >= 18 || edad < 20
+if (mayorDeEdad) {
+  console.log("A")
+  if (5>3) {
+    console.log("B")
+  } else {
+    console.log("B2")
+  }
+} else {
+  console.log("C")
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+//CONDICIONALES MULTIPLES
+// Primero declaramos una variable
+let edad = 29
+if (edad >= 18 && edad < 65) {
+  console.log("A")
+} else {
+  console.log("B")
+}
